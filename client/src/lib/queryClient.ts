dfa90b49a438e3ed8bd3d23f9dc75b56004a3527
Mueen <105032903+WhatsMueenUpto@users.noreply.github.com>
2025-07-30 23:16:57 +0530

    Update queryClient.ts

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -12,7 +12,7 @@ export async function apiRequest(
   url: string,
   data?: unknown | undefined,
 ): Promise<Response> {
-  const res = await fetch(url, {
+  const res = await fetch('https://sociapulsepro-backend.onrender.com/api/mentions', {
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
